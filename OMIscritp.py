8d906408d9fabce5ff57c8f44824734a8dccecfd
Mariano <mariano@vauxoo.com>
2016-11-14 18:23:23 -0600

    New problem, ids list not ready

--- OMIscritp.py
+++ OMIscritp.py
@@ -15,16 +15,19 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-u", "--user",
 					metavar = 'user',
-					help = "The username",)
+					help = "The username",
+					default = 'admin',)
 
 parser.add_argument("-p", "--password",
 					metavar = 'password',
-					help = 'The password',)
+					help = 'The password',
+					default = 'admin',)
 
 parser.add_argument('-i', '--instance_url',
 					metavar = 'url',
 					help = '''This is the instance url/ip.
-					\nExample: odoo.com | 10.209.1.44''',)
+					\nExample: odoo.com | 10.209.1.44''',
+					default = 'localhost',)
 
 parser.add_argument('--port',
 					metavar = 'port_number',
@@ -39,7 +42,11 @@ parser.add_argument('--database',
 
 parser.add_argument("-m", "--module",
 					metavar = 'module',
-					help = 'Uninstall the indicated module',
+					help = 'Uninstall the indicated modules\
+					| Pass it as a list: «example1 example2 example3»\
+					| None comma is needed',
+					type = str,
+					nargs = '+',
 					default = None)
 
 args = vars(parser.parse_args())
@@ -50,10 +57,32 @@ DB = args['database']
 USERNAME = args['user']
 PASSWORD = args['password']
 PORT = args['port']
+MODULES = args['module']
+
 
 connection = xmlrpclib.ServerProxy('http://{0}:{1}/{2}'.format(URL, PORT,
 																COMMONPATH))
 uid = connection.login(DB, USERNAME, PASSWORD)
+connection = xmlrpclib.ServerProxy('http://{0}:{1}/{2}'.format(URL, PORT,
+																OBJECTPATH))
+print connection,"<<<<<<"
+
+
+print connection.execute_kw(DB, uid, PASSWORD, 'ir.module.module', 'search',
+								[('name', '=', MODULES[0])])
+#module_id = [connection.execute_kw(DB, uid, PASSWORD, 'ir.module.module',
+#									'search',[('name', '=', MODULE)]) for MODULE
+#			in MODULES] 
+#module_id = connection.execute_kw(DB, uid, PASSWORD, 'ir.module.module', 'search',
+#								[('name', '=', MODULES)])
+print module_id, "<<"
+
+#install = connection.execute_kw(DB, uid, PASSWORD, "ir.module.module",
+#							"button_immediate_install", MODULES)
+#print install
+
+
+
 
 """
 connection = xmlrpclib.Serverproxy("http://{0}:{1}/{2}".format(URL, PORT,
